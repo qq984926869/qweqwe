7fb37096d731a75403accb5e5ad68a15064f8049
jason <qlfreesky@gmail.com>
2020-12-23 11:16:11 +0800

    Revert "do delete welcome statement"
    This reverts commit dac24af0

--- 01-javase/src/com/cy/java/basic/HelloWorld.java
+++ 01-javase/src/com/cy/java/basic/HelloWorld.java
@@ -14,6 +14,6 @@ public class HelloWorld {
         for(int i=0;i<args.length;i++){
             System.out.println(args[i]);
         };
-        //System.out.println("welcome");
+        System.out.println("welcome");
     }
 }
