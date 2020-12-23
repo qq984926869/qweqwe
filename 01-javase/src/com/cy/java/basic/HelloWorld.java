be529d3cb09c5133ccbf655c67debee63d5cd697
jason <qlfreesky@gmail.com>
2020-12-23 11:17:07 +0800

    do delete hellworld statement

--- 01-javase/src/com/cy/java/basic/HelloWorld.java
+++ 01-javase/src/com/cy/java/basic/HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld {
     /**
      * main方法是程序解释执行的入口方法
      * @param args main方法运行时,由jvm传入的运行数据
-     *
      * JVM 参数
      * -XX:+TraceClassLoading 通过此参数可以检查类的加载过程(
      *  就是将类读到内存的过程)
